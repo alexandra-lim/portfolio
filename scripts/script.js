d17e37cf158e3f32bba8b44d65a8333a53a03955
Alexandra Lim <alexandra.lim@gmail.com>
2019-09-23 15:05:44 -0400

    added animate on scroll and scroll-to-top

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,30 @@
 // smooth scroll by tania rascia
-$("a[href*='#']").click(function(e) {
-	e.preventDefault();
 
-	$("body, html").animate(
-		{
-			scrollTop: $($(this).attr("href")).offset().top
-		},
-		800,
-		"linear"
-	);
+$(document).ready(function(){
+	AOS.init();
+		
+	$("a[href*='#']").click(function(e) {
+		e.preventDefault();
+		$("body, html").animate(
+			{
+				scrollTop: $($(this).attr("href")).offset().top
+			},
+			800,
+			"linear"
+		);
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$(".backToTop").fadeIn();
+		} else {
+			$(".backToTop").fadeOut();
+		}
+	});
+
+	$(".backToTop").click(function() {
+		$("html, body").animate({ "scrollTop": 0 }, 700);
+		return false;
+	});
+
 });
